d3e4f64713b42dcb3a0acfc1cc4483ff6a14785d
RedFiveDead <michaelehale@yahoo.com>
2016-06-24 13:48:24 -0700

    movies and years exercise

---
+++ movies_years.rb
@@ -0,0 +1,9 @@
+movies = { Unborken: 2014,
+          Restrepo: 2010,
+          Lebanon: 2009,
+          Flandres: 2006 }
+
+puts movies[:Unborken]
+puts movies[:Restrepo]
+puts movies[:Lebanon]
+puts movies[:Flandres]
