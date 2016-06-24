ff4f4265d313d7bd55908271bab2a8f7cd871c20
RedFiveDead <michaelehale@yahoo.com>
2016-06-24 14:22:14 -0700

    Years array, name exercise

---
+++ movies_years_array.rb
@@ -0,0 +1,6 @@
+dates = [2014, 2010, 2009, 2006]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
---
+++ name.rb
@@ -0,0 +1,20 @@
+puts "What is your name?"
+  first_name = gets.chomp
+puts "what is your last name?"
+  last_name = gets.chomp
+puts "Hello, your full name is " + first_name + " " + last_name
+
+puts "And how old are you?"
+  age = gets.chomp.to_i
+puts "In 10 years you will be:"
+puts age + 10
+puts "In 10 years you will be:"
+puts age + 20
+puts "In 10 years you will be:"
+puts age + 20
+puts "In 10 years you will be:"
+puts age + 20
+
+10.times do
+  puts first_name
+end
